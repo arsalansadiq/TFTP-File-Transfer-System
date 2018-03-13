25054466de0687987c7690056862dea783dd559b
harshran <25354975+harshran@users.noreply.github.com>
2018-03-12 23:36:59 -0400

    ops

--- TFTP-Network/src/ClientConnectionThread.java
+++ TFTP-Network/src/ClientConnectionThread.java
@@ -335,6 +335,7 @@ public class ClientConnectionThread implements Runnable {
 								+ checkBlock);
 				if (blockNumber != checkBlock) {
 					blockErrorOccurred();
+					blockNumber--;
 				}
 			}
 
